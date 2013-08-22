580081da4c3bf45ede48891a4fa36b39967dca4d
rattapon <rattapon.s@ssd.co.th>
2013-08-22 10:39:49 +0700

    Add a reference for LangExt

--- LangExtExample/LangExtExample/Program.cs
+++ LangExtExample/LangExtExample/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LangExt;
 
 namespace LangExtExample
 {
@@ -10,6 +11,16 @@ namespace LangExtExample
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("======== Func ========");
+            var funcId1 = Func.Id(1);
+            var funcId2 = Func.Id("str1");
+            Console.WriteLine("Func.Id(1) is type of {0}", funcId1.GetType());
+            Console.WriteLine(@"Func.Id(""str1"") is type of {0}", funcId2.GetType());
+
+
+            Console.ReadLine();
+
         }
     }
 }
